5224f39e4c0649fb31519b8760b11aea583d9ba5
JosyPraveena <josypraveena@gmail.com>
2020-08-11 10:52:46 +0200

    netlify deploy changes

---
+++ build/precache-manifest.d36ae7e823f80b27f9fa2edfeaae4322.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1070d0f1f5d763911804ece41db75416",
+    "url": "/index.html"
+  },
+  {
+    "revision": "514aae818a4eccc2ab8c",
+    "url": "/static/css/main.5024595d.chunk.css"
+  },
+  {
+    "revision": "bee7fdb130ca3327e0f4",
+    "url": "/static/js/2.ef108416.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ef108416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514aae818a4eccc2ab8c",
+    "url": "/static/js/main.9a9f2f73.chunk.js"
+  },
+  {
+    "revision": "2457032319f5300d4550",
+    "url": "/static/js/runtime-main.5becae93.js"
+  },
+  {
+    "revision": "f8ff0a1905198e72324daaeb2b5a8d70",
+    "url": "/static/media/Capture.f8ff0a19.PNG"
+  },
+  {
+    "revision": "20178c884954175262bab7be72e63e3c",
+    "url": "/static/media/Josy_Praveena_Prabhagaran_CV.20178c88.pdf"
+  },
+  {
+    "revision": "e5e9c923129472de9d2a82ff9f7bae3a",
+    "url": "/static/media/comicstore.e5e9c923.PNG"
+  },
+  {
+    "revision": "245d95b8d9bbd064d6d0aa719800a626",
+    "url": "/static/media/cookbook.245d95b8.png"
+  },
+  {
+    "revision": "9bbff8621d7743ed4b46f7841ae17123",
+    "url": "/static/media/linkedin.9bbff862.jpg"
+  },
+  {
+    "revision": "9920c74bd5e480c0efe432f84ea50c56",
+    "url": "/static/media/localweather.9920c74b.png"
+  },
+  {
+    "revision": "63238873cf5728e7fffb38a77dabefb2",
+    "url": "/static/media/reover.63238873.PNG"
+  },
+  {
+    "revision": "0c369d65e72defa49ee778620701cc6d",
+    "url": "/static/media/todo.0c369d65.png"
+  },
+  {
+    "revision": "68f4459d722fc42db50680637a14fe67",
+    "url": "/static/media/whatsmyip.68f4459d.png"
+  }
+]);
